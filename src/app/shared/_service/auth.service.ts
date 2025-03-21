9763123c64ed06c6eb48083894c1e71859618706
DEEPK SHARMA <deepakkrsharma5233@gmail.com.com>
2025-03-21 13:02:21 +0530

    Done

--- src/app/shared/_service/auth.service.ts
+++ src/app/shared/_service/auth.service.ts
@@ -11,20 +11,12 @@ import { User } from '../_model/user_model';
 })
 export class AuthService {
 
-  // URL to a server-side endpoint that returns the user's IP address
-  private ipLookupUrl = 'https://api.ipify.org/?format=json';
-
-  // Define the range of office IP addresses
-  private officeIpRanges: string[] = ['49.37.25.44'];
-
-
-  // Sign in with email and password
   private apiUrl = 'https://backend.aquaxa.in/';
 
   constructor(private http: HttpClient, private tokenStorage: TokenStorageService) { }
 
   login(identifier: string | number, pin: string): Observable<User> {
-    return this.http.get<User[]>(`${this.apiUrl}?stype=USERDETAIL&dbase=aquaxa2425&uname=admin`).pipe(
+    return this.http.get<User[]>(`${this.apiUrl}?stype=USERDETAIL&dbase=aquaxa2425&uname=${identifier}`).pipe(
       map(users => {
         const user = users.find(
           u => (u.id === identifier || u.user_name === identifier || u.user_phone === identifier) && u.pin === pin
